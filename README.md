c2a18929b8d3dabbebc1cfffa0675abbfc722a54
Повелеватель Пятачков <va00iosd@gmail.com>
2017-11-12 02:29:14 +0500

    autodocs init and list docs

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# libadsc
+Algorithms and data structures for C
+====================================
 
 C library for algorithms and data structures.  
 For more information about API you can see:
  * tests (in the same folder as it sounds)
  * source code
  * *examples* (not ready)
- * *API docs* (not ready)
+ * API docs (in the `docs` folder)
 
 ## Tests
 
@@ -27,3 +28,15 @@ To install adsc you need to execute shell comands:
  * `cmake .. -DCMAKE_BUILD_TYPE=Release`
  * `make -j4`
  * `sudo make install`
+
+## Docs
+
+To generate documentation for ADSC you need to install `doxygen`.
+In example, you can do it on ubuntu: `sudo apt-get install doxygen`.
+
+To generate documentation execute shell comands:
+ * `cd docs`
+ * `doxygen config.conf`
+
+Then, you can see documentation by opening file `docs/html/index.html` in your web browser.  
+In most cases you will need to open tab **Modules**. All usefull information will be defined here in groups.
--- include/adsc.h
+++ include/adsc.h
@@ -15,78 +15,133 @@
 #define ads_max(a, b) ((a) > (b) ? (a) : (b))
 #define ads_min(a, b) ((a) < (b) ? (a) : (b))
 
-
 // -------------------------------- list --------------------------------
+/**
+ * \defgroup List
+ * @{
+ */
 
-/** list data */
+/** @brief Linked list data. */
 typedef struct
 {
     void* next;
     char value;
 } ads_list_data_t;
 
-/** list header */
+/** @brief Linked list header. */
 typedef struct
 {
     ads_list_data_t* head;
     size_t size;
 } ads_list_t;
 
-/** creates linked list with default allocator (on heap) */
+/** 
+ * @brief Creates linked list with default allocator (on heap). 
+ * @note You should use #ads_list_destroy when list is not needed.
+ */
 ads_list_t* ads_list_create();
 
-/** initialises linked list without allocating memory */
+/**
+ * @brief Initialises linked list.
+ * @param list  Pointer to allocated linked list.
+ */
 void ads_list_init(ads_list_t* list);
 
-// FIXME: what if we need to insert some element in some place in list,
-//        and what if need to pop some element in some place in list?
-
-/** adds some data to linked list */
+/**
+ * @brief  Inserts element in linked list (makes copy of @p value).
+ *
+ * @param  list  Pointer to linked list.
+ * @param  value Pointer to user data which user wants to
+ *               insert (may be @c NULL).
+ * @param  size  Size of user's @p value in bytes.
+ * @return Pointer to value of created list element.
+ *
+ * @note   User's value will be copied from pointer's value if
+ * value not @c NULL. Otherwise, this function only allocates memory
+ * for list element, and returns pointer where user can store
+ * actual value.
+ */
 void* ads_list_push(ads_list_t* list, void* value, size_t size);
 
-/** returns head element if list (NULL if empty) */
+/**
+ * @brief  Shows head element of linked list.
+ * @param  list  Pointer to linked list.
+ * @return Pointer to value of head element, if it exists.
+ *         Otherwise, @c NULL.
+ */
 void* ads_list_top(ads_list_t* list);
 
-/** returns pointer to head element (NULL if empty) */
+/**
+ * @brief  Pops head element of linked list.
+ * @param  list   Pointer to linked list.
+ * @param  where  Pointer user allocated memory where head element 
+ *                of linked list will be copied.
+ * @param  size   Size of element user wants to get.
+ * @note   If list is empty then nothing will happend.
+ */
 void ads_list_pop(ads_list_t* list, void* where, size_t size);
 
-/** returns true, if list is empty */
+/**
+ * @brief  Returns @c TRUE if list is empty.
+ * @param  list   Pointer to linked list.
+ */
 bool ads_list_empty(ads_list_t* list);
 
-/** clear list if data is pod */
+/**
+ * @brief  Clears all list data (then list will be empty).
+ * @param  list   Pointer to linked list.
+ * @note   All user data will be destroyed as pod values.
+ */
 void ads_list_clear_base(ads_list_t* lst);
 
 /**
- * destroys any list content using remover, and list itself;
- * sets list value to NULL
+ * @brief  Destroys list if it was allocated with #ads_list_create
+ *         
+ * May be used instead of #ads_list_clear_base, if list itself
+ * is not needed anymore.
+ * 
+ * @param  list   Pointer to pointer of linked list.
+ * @note   Makes list not able to reuse (sets @c *list to @c NULL).
  */
 void ads_list_destroy(ads_list_t** list);
 
 /**
- * iterates with apply block of operations to iterator element.
+ * @brief Iterates with apply block of operations to iterator element.
  *
- * example for ads_list_t* lst:
+ * Example for list of @c float:
+ * @code
+ *   ads_list_t* lst;
+ *   ...
  *   float sum = 0.0f;
  *   ads_list_for(lst, it)
  *   {
  *       sum += ads_list_val(lst, it, float);
  *   }
+ * @endcode
  */
 #define ads_list_for(lst, iter)                                           \
     for (ads_list_data_t* iter = (lst)->head; iter != NULL;               \
          iter = iter->next)
 
+/**
+ * @brief Gets referens to list value from iterator.
+ * @see ads_list_for
+ */ 
 #define ads_list_val(iter, type) (*(type*) &(iter->value))
 
 /**
- * iterates with apply only one simple operation.
+ * @brief Iterates with apply only one simple operation.
  *
- * example for ads_list_t* lst (list of int):
+ * Example for list of @c int:
+ * @code
+ *   ads_list_t* lst;
+ *   ...
  *   int sum = 0;
  *   ads_list_for_each(
  *       lst, it,
  *       sum += ads_list_val(it, int)
  *   );
+ * @endcode
  */
 #define ads_list_for_each(lst, iter, op)                                  \
     for (ads_list_data_t* iter = (lst)->head; iter != NULL;               \
@@ -94,8 +149,13 @@ void ads_list_destroy(ads_list_t** list);
         op;
 
 /**
- * example for ads_list_t* lst, and elements allocated with malloc:
- *   ads_list_clear(lst, free);
+ * Example for list elements allocated with @c malloc:
+ * @code
+ *   ads_list_t* lst = ads_list_create();
+ *   int* ptr = ads_list_push_value(lst, void*, malloc(10));
+ *   ...
+ *   ads_list_clear(lst, free, void*);
+ * @endcode
  */
 #define ads_list_clear(lst, deleter, type)                                \
     {                                                                     \
@@ -103,22 +163,28 @@ void ads_list_destroy(ads_list_t** list);
         {                                                                 \
             type val;                                                     \
             ads_list_pop(lst, &val, sizeof(type));                        \
-            deleter(&val);                                                \
+            deleter(val);                                                 \
         }                                                                 \
     }
 
 /**
- * example for ads_list_t* lst:
+ * Example for @c ads_list_t*:
+ * @code
  *   ads_list_push_value(lst, int, 228);
  *   ads_list_push_value(lst, point2f_t, { 3.4f, 0.3f });
  *   ...
  *   ads_list_destroy(lst, free);
+ * @endcode
  */
 #define ads_list_push_value(lst, type, el)                                \
     *(type*) ads_list_push(lst, NULL, sizeof(type)) = el
 
-
+/**@}*/
 // -------------------------------- dlist --------------------------------
+/**
+ * \defgroup DList
+ * @{
+ */
 
 /** dlist data */
 typedef struct
@@ -185,7 +251,12 @@ void ads_dlist_destroy(ads_dlist_t** list);
 
 #define ads_dlist_val(iter, type) (*(type*) &(iter->value))
 
+/**@}*/
 // ----------------------------- alloc pool -----------------------------
+/**
+ * \defgroup AllocPool
+ * @{
+ */
 
 typedef struct
 {
@@ -209,9 +280,13 @@ void ads_alloc_pool_init(ads_alloc_pool_t* self,
 void ads_alloc_pool_clear(ads_alloc_pool_t* self);
 void ads_alloc_pool_destroy(ads_alloc_pool_t** self);
 
-
+/**@}*/
 // ------------------------- alloc pool list ----------------------------
 // FIXME: not ready to use
+/**
+ * \defgroup AllocPoolList
+ * @{
+ */
 
 typedef struct
 {
@@ -233,3 +308,5 @@ void ads_alloc_pool_list_init(ads_alloc_pool_list_t* self,
                               size_t capacity);
 void ads_alloc_pool_list_clear(ads_alloc_pool_list_t* self);
 void ads_alloc_pool_list_destroy(ads_alloc_pool_list_t** self);
+
+/**@}*/
